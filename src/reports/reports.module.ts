7cd530040e3609e069c02e4335e062749eafc516
Sargis <terbalyantss@gmail.com>
2024-11-22 20:08:56 +0400

    Update reports.module.ts

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,8 +1,18 @@
 import { Module } from "@nestjs/common";
-import { ReportsService } from "./reports.service";
 import { ReportsController } from "./reports.controller";
+import { ReportsService } from "./reports.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ReportsModel, ReportsSchema } from "./entities/report.entity";
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: ReportsModel.name,
+                schema: ReportsSchema,
+            },
+        ]),
+    ],
     controllers: [ReportsController],
     providers: [ReportsService],
 })
